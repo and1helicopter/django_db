c5259213aeaf32e52cf841a9cac8b278230b07a5
and1helicopter <and1helicopter@mail.ru>
2016-03-25 03:40:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,11 @@ sudo /etc/init.d/gunicorn restart
 cd ask/ask/
 gunicorn -b 0.0.0.0:8000 wsgi
 
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'and1helicopter' IDENTIFIED BY 'and1helicopter'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'and1helicopter'"
+
 
 
 
